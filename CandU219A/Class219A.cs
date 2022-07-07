541ea8b24bb5760e70fb8af976c08c629ff918a4
J Rockwell Seebach <jrs@ironpinephoto.com>
2022-07-07 14:02:08 -0700

    Add project files.

---
+++ CandU219A/Class219A.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CandU219A
+{
+    class Class219A
+    {
+        public static void MathOP(int X, int Z = 0)
+        {
+            int answer = (7 + X + Z);
+            Console.WriteLine(answer);
+        }
+    }
+}
---
+++ CandU219A/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CandU219A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int X = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Please enter another optional number \n" +
+                "Or press 'Enter to continue");
+
+            string Y = (Console.ReadLine());
+
+            if (string.IsNullOrEmpty(Y))
+            {
+
+                Class219A.MathOP(X);
+                Console.ReadLine();
+            }
+            else
+            {
+                int Z = Convert.ToInt16(Y);
+                Class219A.MathOP(X, Z);
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
+
+
+
